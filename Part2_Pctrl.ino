7a22b26010f0e3f6e425b9ce4070b651c44f29c0
Crush0313 <127765810+Crush0313@users.noreply.github.com>
2023-08-17 12:38:34 +0900

    Update Part2_Pctrl.ino
    recent

--- Part2_Pctrl.ino
+++ Part2_Pctrl.ino
@@ -1,29 +1,65 @@
+//모터11 유성17 몸통 46
+
+//백색 LOW 선
 #define dirR 12 //모터A 방향
 #define dirL 13 //모터B 방향
 #define R 10    //모터A 속도
 #define L 11    //모터B 속도
+#define MX_Speed 110
+#define Kp 140 //블루투스로 값 바꿀건지, 매번 연결해 바꿀지 결정
 
 //모터 외부전원 시 OPT 점퍼캡 제거 필수
 
 bool isOnLine[5];
-int IR[5] = {2,3,4,5,6}
+int IR[5] = {2,3,4,5,6}; //기본 센서
+int IR2[2] = {7,8}; //마크 체크용 센서
 int count;
 int sum;
 
+long time;
+int MK = 0;
+bool StopMV = false;
+
 float error = 0;
-#define Kp 40 //블루투스로 값 바꿀건지, 매번 연결해 바꿀지 결정
-int P;
+float P;
 
 void setup() {
   pinMode(dirR, OUTPUT); // DC모터의 방향제어 pinMode를 설정합니다.
-  digitalWrite(dirR, LOW); // DC모터의 회전 방향을 정해줍니다.//회로 연결상태에 따라서 LOW가 전진일지 HIGH가 전진인지 정해집니다.
+  digitalWrite(dirR, HIGH); // DC모터의 회전 방향을 정해줍니다.//회로 연결상태에 따라서 LOW가 전진일지 HIGH가 전진인지 정해집니다.
   
   pinMode(dirL, OUTPUT); // DC모터의 방향제어 pinMode를 설정합니다.
   digitalWrite(dirL, LOW); // DC모터의 회전 방향을 정해줍니다.
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  time = millis();
+  //ChkMK();
   chkIR();
+  Move();
+}
+
+void ChkMK(){
+  //시작후 10초 경과시에만 작동(시작 라인 무시)
+  if(time>=10000){
+    MK = 0;
+    if(digitalRead(IR2[0])==LOW){
+      MK++;
+    }
+    if(digitalRead(IR2[1])==LOW){
+      MK++;
+    }
+
+    if(MK>=2){
+    if(digitalRead(IR[0]) == LOW || digitalRead(IR[4]) == LOW){
+        return;
+      }
+      else{
+          StopMV = true;
+      }
+    }
+  }
 }
 
 // 위치에 따른 각 IR의 값
@@ -44,18 +80,19 @@ void loop() {
 
 //for문 돌면서 적외선 센서값 측정해서, error 산출
 void chkIR(){
+
   //1. 변수 초기화
   error = 0;
   count = 0;
-  cum = 0;
+  sum = 0;
 
-  //2. 적외선 센서값 읽기, 
-  for (int i = 0; i <= 5; i++){
+  //2. 적외선 센서값 읽기, 흰 선이면 LOW
+  for (int i = 0; i < 5; i++){
 
-    if(digitalRead(IR[i]) == HIGH){
-      sum += (2-i) //-2 -1 0 1 2
+    if(digitalRead(IR[i]) == LOW){
+      sum += (i-2); //-2 -1 0 1 2
       count ++;
-    }; 
+    }
   }
   
   //3. error 값 생성
@@ -67,27 +104,48 @@ void chkIR(){
   else{
     error = sum;
   }
+  Serial.println(error);
 }
 
 void Move(){
+  if(!StopMV){
+      digitalWrite(dirR, HIGH);
+      digitalWrite(dirL, LOW);
 
-  //직진이면 그냥 풀악셀
-  if(error==0){
-      analogWrite(R, 255);
-      analogWrite(L, 255);
-  }
+    //직진이면 그냥 풀악셀
+    if(error==0){
 
-  //error가 음수, 왼쪽에서 선 감지, 왼바퀴 감속
-  else if(error < 0){
-    P = -error * Kp
-    analogWrite(R, 255);
-    analogWrite(L, 255 - P);
-  }
+        analogWrite(R, MX_Speed);
+        analogWrite(L, MX_Speed);
+    }
 
-  //error가 양수, 오른쪽에서 선 감지, 오른바퀴 감속
-  else if(error < 0){
-    P = error * Kp
-    analogWrite(R, 255 - P);
-    analogWrite(L, 255);
+    //error가 음수, 왼쪽에서 선 감지, 왼바퀴 감속
+    else if(error < 0){
+      P = -error * Kp;
+
+      analogWrite(R, MX_Speed);
+
+      if(P > MX_Speed){
+        digitalWrite(dirL, HIGH);
+        analogWrite(L, 200);
+      }
+      else{
+        analogWrite(L, P - MX_Speed);
+      }
+    }
+
+    //error가 양수, 오른쪽에서 선 감지, 오른바퀴 감속
+    else if(error > 0){
+      analogWrite(L, MX_Speed);
+
+      P = error * Kp;
+      if(P > MX_Speed){
+        digitalWrite(dirR, LOW);
+        analogWrite(R, P - MX_Speed);
+      }
+      else{
+        analogWrite(R, MX_Speed - P);
+      }
+    }
   }
 }
